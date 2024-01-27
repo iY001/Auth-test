fe44807e3b425f86a6308263f3493b2d01a277fe
iY001 <youssefabdo4005@gmail.com>
2024-01-27 21:15:41 +0200

    --fix

--- public/env.js
+++ public/env.js
@@ -5,7 +5,7 @@ window.env = {
   "APPDATA": "C:\\Users\\youss\\AppData\\Roaming",
   "ChocolateyInstall": "C:\\ProgramData\\chocolatey",
   "ChocolateyLastPathUpdate": "133419557884457482",
-  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_7816_IMANZMDLYWKWUCVA",
+  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_5480_VJBKPMONDCYQYYHI",
   "COLOR": "1",
   "COLORTERM": "truecolor",
   "CommonProgramFiles": "C:\\Program Files\\Common Files",
