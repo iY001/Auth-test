889c2232c7f946718bc421c8234c5b91a4fd70c9
iY001 <youssefabdo4005@gmail.com>
2024-01-29 19:29:02 +0200

    --Done

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -59,38 +59,40 @@ function Profile() {
   return (
     <>
 
-      <p className='text-4xl text-red-500 font-bold text-center mt-4'>{Massege ? Massege : ''}</p>
-      
-      {
-        currentUser ?
-          <div className='mx-4'>
-            <h1 className='text-4xl mt-6 text-orange-500'>Profile</h1>
+      <div className='flex flex-col justify-center md:text-left text-center'>
+        <p className='text-4xl text-red-500 font-bold text-center mt-4'>{Massege ? Massege : ''}</p>
 
-            <div>
-              <p className='text-2xl my-2'>Name : {currentUser.displayName}  {showForm ? <button onClick={() => { setShowForm(false) }} className='mx-2 px-4 py-2 text-sm bg-red-700 hover:bg-red-600 duration-300 text-white'>Close</button> : <button onClick={() => setShowForm(true)} className='mx-2 px-4 py-2 text-sm bg-green-600  duration-300 text-white'>Edit</button>} </p>
-              <p className='text-2xl my-2'>Email : {currentUser.email}</p>
+        {
+          currentUser ?
+            <div className='mx-4'>
+              <h1 className='text-4xl mt-6 text-orange-500'>Profile</h1>
 
-              {
-                showForm ? <form onSubmit={handleSetDisplayName} className='my-4'>
-                  <label className='text-xl'>Set Name : </label>
-                  <input className='border-2 border-gray-300 px-2 py-2' type="text" ref={displayNameRef} placeholder='New Name' />
-                  <button type="submit" className='mx-2 px-6 py-2 bg-green-600 hover:bg-green-500 duration-300 text-white'>Set</button>
-                </form>
-                  : null
-              }
+              <div>
+                <p className='text-2xl my-2'>Name : {currentUser.displayName}  {showForm ? <button onClick={() => { setShowForm(false) }} className='mx-2 px-4 py-2 text-sm bg-red-700 hover:bg-red-600 duration-300 text-white'>Close</button> : <button onClick={() => setShowForm(true)} className='mx-2 px-4 py-2 text-sm bg-green-600  duration-300 text-white'>Edit</button>} </p>
+                <p className='text-2xl my-2'>Email : {currentUser.email}</p>
 
-              <button onClick={handleDelete} className='mx-2 px-6 py-2 bg-red-700 hover:bg-red-600 duration-300 text-white'>Delete</button>
+                {
+                  showForm ? <form onSubmit={handleSetDisplayName} className='my-4'>
+                    <label className='text-xl'>Set Name : </label>
+                    <input className='border-2 border-gray-300 px-2 py-2' type="text" ref={displayNameRef} placeholder='New Name' />
+                    <button type="submit" className='mx-2 px-6 py-2 bg-green-600 hover:bg-green-500 duration-300 text-white'>Set</button>
+                  </form>
+                    : null
+                }
 
-              <button onClick={handleLogout} className='text-white py-2 px-6 mt-4 bg-red-700 hover:bg-red-600 duration-300'>Logout</button>
-              {error && <p style={{ color: 'red' }}>{error}</p>}
+                <button onClick={handleDelete} className='mx-2 px-6 py-2 bg-red-700 hover:bg-red-600 duration-300 text-white'>Delete</button>
+
+                <button onClick={handleLogout} className='text-white py-2 px-6 mt-4 bg-red-700 hover:bg-red-600 duration-300'>Logout</button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+              </div>
             </div>
-          </div>
-          :
-          <div>
-            <p className='text-4xl text-red-500 font-bold text-center mt-4'>{Massege ? '' : 'You are not logged in'}</p>
-          </div>
-      }
+            :
+            <div>
+              <p className='text-4xl text-red-500 font-bold text-center mt-4'>{Massege ? '' : 'You are not logged in'}</p>
+            </div>
+        }
 
+      </div>
 
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
     <>
       <div className='flex flex-col mt-2 text-3xl space-y-3 justify-center text-center'>
         <p>Home</p>
-        <section className='py-4'>
-          <Link to={"/"} className='py-2 px-6 mx-2 bg-orange-500 text-center text-md text-white'>Home</Link>
-          <Link to={"/signin"} className='py-2 px-6 mx-2 bg-orange-500 text-center text-md text-white'>Login</Link>
-          <Link to={"/signup"} className='py-2 px-6 bg-orange-500 text-center text-md text-white mx-2'>Create An Account</Link>
+        <section className='py-4 flex flex-col md:flex-row justify-center'>
+          <Link to={"/"} className='py-2 my-4 md:my-0 px-6 mx-2 bg-orange-500 text-center text-md text-white'>Home</Link>
+          <Link to={"/signin"} className='py-2 my-4 md:my-0 px-6 mx-2 bg-orange-500 text-center text-md text-white'>Login</Link>
+          <Link to={"/signup"} className='py-2 my-4 md:my-0 px-6 bg-orange-500 text-center text-md text-white mx-2'>Create An Account</Link>
           {
-            currentUser && <Link to={"/profile"} className='py-2 px-6 bg-orange-500 text-center text-md text-white mx-2'>Profile</Link>
+            currentUser && <Link to={"/profile"} className='py-2 my-4 md:my-0 px-6 bg-orange-500 text-center text-md text-white mx-2'>Profile</Link>
           }
 
         </section>
